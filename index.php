e6a9cbc254450a7759bf9c463b5035b211e3e676
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-26 13:21:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -57,23 +57,19 @@
                                 </span>
                             </div>
                             <div id="itemTable" style="margin: 1em; overflow-x:auto;">
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 15%;">Product</th>
-                                            <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 25%;">Name</th>
-                                            <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 15%;">Quantity</th>
-                                            <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 25%;">Unit Price</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;><img src="https://i.imgur.com/YnDcc8J.png"/></td>
-                                            <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Name</td>
-                                            <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Quantity</td>
-                                            <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>RM Price</td>
-                                        </tr>
-                                    </tbody>
+                                <table>                                    
+                                    <tr>
+                                        <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 15%;">Product</th>
+                                        <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 25%;">Name</th>
+                                        <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 15%;">Quantity</th>
+                                        <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 25%;">Unit Price</th>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;><img src="https://i.imgur.com/YnDcc8J.png"/></td>
+                                        <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Name</td>
+                                        <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Quantity</td>
+                                        <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>RM Price</td>
+                                    </tr>                                    
                                 </table>
                             </div>
                         </div>
