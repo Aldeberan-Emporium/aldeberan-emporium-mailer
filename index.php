f91880da45b053093a6376394ff5f4252dc64358
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-26 13:08:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,13 @@
                                     <th scope="col" style="padding: 10px 0px; background: #C7B198; color: #F3F5E3; width: 25%;">Unit Price</th>
                                 </tr>
                             </thead>
-                            <tbody>';
+                            <tbody>
+                            <tr>
+                                <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;><img src="https://i.imgur.com/YnDcc8J.png" style="width: 20%;"/></th>
+                                <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Name</td>
+                                <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Quantity</td>
+                                <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>RM Price</td>
+                            </tr>';
     /*$html .='<tr>
                 <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;><img src="https://i.imgur.com/YnDcc8J.png" style="width: 20%;"/></th>
                 <td style="padding: 10px 0px; background: #EFE4D6; color: #675B4A;>Name</td>
